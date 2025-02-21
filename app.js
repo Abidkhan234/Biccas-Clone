9bcfb2a9884180b3d24a42d0cbb330fb6d600073
Abidkhan234 <abidshahkhan89@gmail.com>
2025-02-22 00:47:24 +0500

    remove unused images and enhance button interaction with smooth transitions

--- app.js
+++ app.js
@@ -17,3 +17,55 @@ const openSidebar = () => {
 const closeSidebar = () => {
     sideBar.classList.add("translate-x-[100%]");
 }
+
+const allMenuBtn = document.querySelectorAll(".nav-menu a");
+
+allMenuBtn.forEach((v) => {
+
+    v.addEventListener("click", (e) => {
+
+        allMenuBtn.forEach(element => {
+            element.classList.remove("check")
+        });
+
+        e.target.classList.add("check")
+
+    })
+
+})
+
+
+const subscriptionBtns = document.querySelectorAll(".subscription-btns button");
+
+const btn1 = document.getElementById("btn-1");
+
+const btn2 = document.getElementById("btn-2");
+
+btn1.addEventListener("click", () => {
+    return showingResult(btn1.innerText);
+})
+
+btn2.addEventListener("click", () => {
+    return showingResult(btn2.innerText);
+})
+
+const values = [
+    "Bil Monthly",
+    "Bil Yearly"
+]
+
+const showingResult = (testingValue) => {
+    if (testingValue === values[0]) {
+
+        btn1.classList.add("clicked")
+
+        btn2.classList.remove("clicked")
+
+    } else if (testingValue === values[1]) {
+
+        btn2.classList.add("clicked")
+
+        btn1.classList.remove("clicked")
+
+    }
+}
