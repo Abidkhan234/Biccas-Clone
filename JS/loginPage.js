9fb793d2abedf21f76e4feef2e0addd26fe29ca9
Abidkhan234 <abidshahkhan89@gmail.com>
2025-02-24 21:16:49 +0500

    enhance login page with password visibility toggle and validation checks

--- JS/loginPage.js
+++ JS/loginPage.js
@@ -6,11 +6,61 @@ const container = document.querySelector(".main-container");
 
 loginBtn.addEventListener("click", () => {
     container.classList.add("check");
-    console.log("ok");
-    
 })
 
 
 signBtn.addEventListener("click", () => {
     container.classList.remove("check");
 })
+
+
+// For password check
+
+const passwordEyeBtn = document.querySelectorAll(".password-eye-btn");
+
+passwordEyeBtn.forEach((v) => {
+    v.addEventListener("click", () => passwordChecker(v));
+})
+
+const passwordChecker = (v) => {
+
+    let i = v.querySelector("i");
+
+    let passwordBox = v.parentElement.parentElement.querySelector(".password")
+
+    if (i.classList.contains("fa-eye-slash")) {
+
+        passwordBox.type = "text";
+        i.classList.replace("fa-eye-slash", "fa-eye");
+
+    } else {
+
+        passwordBox.type = "password";
+        i.classList.replace("fa-eye", "fa-eye-slash");
+
+    }
+
+}
+
+// For password check
+
+
+// For login and sign in fields check
+
+const loginFormBtn = document.getElementById("login-btn");
+
+const signInFormBtn = document.getElementById("signIn-btn");
+
+let uid = localStorage.getItem("uid");
+
+loginFormBtn.addEventListener("click", () => {
+    if (uid) {
+        
+    } else {
+        alert("Plz create account first");
+        container.classList.add("check");
+        return;
+    }
+})
+
+// For login and sign in fields check
